ab8f3d7b49832a2fb2396d15065ae1853c1be2b3
Rohit Thorbole <rohitthorbole4@gmail.com>
2026-08-26 19:46:18 +0530

    Adding Policy vars

--- module/variables.tf
+++ module/variables.tf
@@ -7,3 +7,8 @@ variable "iam_user_name" {
   type = string
   description = "Name of the IAM user to create. Required if create_iam_user is true."
 }
+
+variable "iam_policy" {
+  type = string
+  description = "The IAM policy to attach to the user. Required if create_iam_user is true."
+}
